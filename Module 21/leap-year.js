a6758223796fe0c76d8b56504580099a1939f785
Mr Khan <mahimkhan53@gmail.com>
2023-02-01 16:24:15 +0600

    leap year

---
+++ Module 21/leap-year.js
@@ -0,0 +1,27 @@
+// *console log version
+
+function leapYear(year){
+    var remainder = year % 4;
+    if (remainder === 0 ){
+        console.log('Leap Year:' , year);
+    }
+    else{
+        console.log('Not Leap Year:', year);
+    }
+}
+ 
+var thisYear = leapYear(2020);
+
+// *return version
+
+function leapYear(year){
+    var remainder = year % 4;
+    if(remainder === 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+var thisYear = leapYear(2023);
+console.log(thisYear);
